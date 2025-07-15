daf625990d1fd29f310c2cfa9312f167b50bc0de
AlexSilvaRibeiro <143361282+alexsilvaribeiro@users.noreply.github.com>
2025-07-15 13:06:32 +0000

    Desafio nível novato: movimentação das peças de xadrez.

--- xadrez.c
+++ xadrez.c
@@ -8,25 +8,45 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
+    printf("\n-----------BISPO-------------------------\n\n");
+
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // Utilizando a estrutura DO-WHILE para implementar a movimentação:
+    int i=0;
+    do
+    {
+        printf("cima + direita %d\n",i+1);
+        i++;
+    } while (i<=4);
+    
+    printf("\n-----------TORRE-------------------------\n");
+
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // Utilizando a estrutura FOR para implementar a movimentação:
+
+    for (int j = 0; j < 5; j++)
+    {
+        printf("direita %d \n",j+1);
+    }
+    
+    printf("-----------RAINHA-------------------------\n");
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    int a=0;
+    while (a<8)
+    {
+        printf("esquerda %d \n",a+1);
+        a++;
+    }
+    
+    printf("\n-----------------------------------------\n");
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    
 
     return 0;
 }
